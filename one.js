8d22e8b44fab41f13003d27f48dd226cffa7ba22
jonwilsoncomedy <jonwilsoncomedy@gmail.com>
2016-07-06 10:13:14 -0500

    Update one.js

--- one.js
+++ one.js
@@ -1,4 +1,4 @@
-// This functions creates a random number between the min(inclusive) and max (exclusive) parameters
+// Creates a random number between the min(inclusive) and max (exclusive) parameters
 
 module.exports = function(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
