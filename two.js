0057bca04ae528b4aaa7f9790da8b41bfb45a935
jonwilsoncomedy <jonwilsoncomedy@gmail.com>
2016-07-06 10:13:47 -0500

    Update two.js

--- two.js
+++ two.js
@@ -1,4 +1,4 @@
-// This function converts the parameter to USD
+// Converts the parameter to USD
 
 module.exports = function(val) {
   return '$' + val.toFixed(2).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
